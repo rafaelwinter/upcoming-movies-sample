55078d0975c084f6e871b3c0eeb9531e3a9df6ac
Rafael Winter <rafael.winter@gmail.com>
2018-03-27 16:26:57 -0300

    Open the Details screen

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.arctouch.codechallenge.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.arctouch.codechallenge.R
 import com.arctouch.codechallenge.api.TmdbApi
 import com.arctouch.codechallenge.base.BaseActivity
 import com.arctouch.codechallenge.data.Cache
+import com.arctouch.codechallenge.details.DetailsActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.home_activity.*
@@ -23,7 +25,13 @@ class HomeActivity : BaseActivity() {
                 val moviesWithGenres = it.results.map { movie ->
                     movie.copy(genres = Cache.genres.filter { movie.genreIds?.contains(it.id) == true })
                 }
-                recyclerView.adapter = HomeAdapter(moviesWithGenres)
+
+                recyclerView.adapter = HomeAdapter(moviesWithGenres) {
+                    val intent = Intent(this, DetailsActivity::class.java)
+                    intent.putExtra(DetailsActivity.EXTRA_MOVIE_ID, it.id.toLong())
+                    startActivity(intent)
+                }
+
                 progressBar.visibility = View.GONE
             }
     }
