a5b20355a94e28781e24a9e3bd2982ede0429638
Rafael Winter <rafael.winter@gmail.com>
2018-03-28 16:03:34 -0300

    Create the CodeChallengeApplication class

---
+++ app/src/main/java/com/arctouch/codechallenge/CodeChallengeApplication.kt
@@ -0,0 +1,9 @@
+package com.arctouch.codechallenge
+
+import android.app.Application
+
+class CodeChallengeApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
